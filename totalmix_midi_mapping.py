92ff915d14ff957326a0ec5964dc620aa706d071
Andreas Einsiedler <hello@andreaseinsiedler.com>
2024-07-07 20:34:56 +0200

    allmost finished solo and mute logi

--- totalmix_midi_mapping.py
+++ totalmix_midi_mapping.py
@@ -9,27 +9,62 @@ from rtmidi.midiutil import open_midioutput
 from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
 from rtmidi.midiconstants import (CONTROL_CHANGE)
 
-def banking(row):
+def banking(row, ascii_char_old):
 
     #row = "Ou"
     send = True
-    command = 0x28
+    command_up = 0x29
+    command_dn = 0x28
+    ascii_char = ascii_char_old
+    print("row:", row, "ascii_char_old:",ascii_char_old, "send:", send)
 
     while True:
 
+        if ascii_char_old[:2] == row[:2]:
+            break
+
         if send:
 
-            msgout = ([NOTE_OFF | 0, command, 0])
+            if row[:2] == "In" and ascii_char_old[:2] == "Pb":
+                msgout = ([NOTE_OFF | 0, command_dn, 0])
+                print("command_dn")
+                break
+
+            elif row[:2] == "In" and ascii_char_old[:2] == "Ou":
+                msgout = ([NOTE_OFF | 0, command_up, 0])
+                print("command_up")
+                break
+
+            elif row[:2] == "Pb" and ascii_char_old[:2] == "In":
+                msgout = ([NOTE_OFF | 0, command_dn, 0])
+                print("command_dn")
+                break
+
+            elif row[:2] == "Pb" and ascii_char_old[:2] == "Ou":
+                msgout = ([NOTE_OFF | 0, command_dn, 0])
+                print("command_dn")
+                break
+
+            elif row[:2] == "Ou" and ascii_char_old[:2] == "In":
+                msgout = ([NOTE_OFF | 0, command_dn, 0])
+                print("command_dn")
+                break
+
+            elif row[:2] == "Ou" and ascii_char_old[:2] == "Pb":
+                msgout = ([NOTE_OFF | 0, command_up, 0])
+                print("command_dn")
+                break
+
+            else:
+                msgout = ([NOTE_OFF | 0, command_up, 0])
+                print("else__command_up")
+
 
             midiout.send_message(msgout)
 
             send = False
 
-
-
         msg1 = midiin_1.get_message()
-
-
 
         if msg1:
 
@@ -37,11 +72,10 @@ def banking(row):
 
             if lcd_header == message[:6]:
 
-                ascii_char = ""
+                #ascii_char = ""
                 for num in message[7:-1]: ascii_char += chr(num)
 
-
-                print("LCD Text -> ", ascii_char, "\n")
+                print("banking - ascii_char", ascii_char, "\n")
 
                 if ascii_char[:2] == row[:2]:
                     break
@@ -50,6 +84,11 @@ def banking(row):
                     send = True
 
         time.sleep(0.0025)
+
+    return ascii_char
+
+
+
 
 if getattr(sys, 'frozen', False):
     # we are running in a bundle
@@ -73,10 +112,20 @@ output_bank_dict = matrix[8]
 submix_prev = 1
 lcd_header = [240, 0, 0, 102, 20, 18]
 
+
+
+
+
+
 #Midi Inputs Init
 
 #log = logging.getLogger('midiin_poll')
 #logging.basicConfig(level=logging.DEBUG)
+
+print("\n##########################################################################\nTotalMix Midi Mapping v0.1 (2024)\nOpen Source Midi Learn Functionality for TotalMix from RME\nTricking the MackieControl Implemantation to gain absolute Midi Mapping.\nBuilt with python 3.9, python-rtmidi, pyinstaller\nAuthor: andreaseinsiedler\nhttps://github.com/andreaseinsiedler/totalmix_midi_mapping\n##########################################################################")
+
+
+
 
 #Midi Input External
 
@@ -84,22 +133,28 @@ lcd_header = [240, 0, 0, 102, 20, 18]
 # is given as the first argument on the command line.
 # API backend defaults to ALSA on Linux.
 
+print("\nChoose the external Input:")
+print("---------------------------\n")
+
 port = sys.argv[1] if len(sys.argv) > 1 else None
 
 try:
-    midiin_0, port_name_in_0 = open_midiinput(port)
-    #midiin_0, port_name_in_0 = open_midiinput(0)
+    #midiin_0, port_name_in_0 = open_midiinput(port)
+    midiin_0, port_name_in_0 = open_midiinput(0)
 
 except (EOFError, KeyboardInterrupt):
     sys.exit()
 
 #Midi Input TotalMix Response
 
+print("\nChoose the Input from TotalMix:\n")
+print("---------------------------\n")
+
 port = sys.argv[1] if len(sys.argv) > 1 else None
 
 try:
-    midiin_1, port_name_in_1 = open_midiinput(port)
-    #midiin_1, port_name_in_1 = open_midiinput(7)
+    #midiin_1, port_name_in_1 = open_midiinput(port)
+    midiin_1, port_name_in_1 = open_midiinput(4)
     midiin_1.ignore_types(sysex=False)
 
 except (EOFError, KeyboardInterrupt):
@@ -114,18 +169,47 @@ except (EOFError, KeyboardInterrupt):
 # is given as the first argument on the command line.
 # API backend defaults to ALSA on Linux.
 
+print("\nChoose the output to TotalMix:\n")
+print("---------------------------\n")
+
 port = sys.argv[1] if len(sys.argv) > 1 else None
 
 try:
-    midiout, port_name_out = open_midioutput(port)
-    #midiout, port_name_out = open_midioutput(7)
+    #midiout, port_name_out = open_midioutput(port)
+    midiout, port_name_out = open_midioutput(4)
 
 except (EOFError, KeyboardInterrupt):
     sys.exit()
 
-print("\nMidi Input 1 (External):", port_name_in_0)
-print("Midi Input 2 (TotalMix):", port_name_in_1)
+print("\nMidi Input from External:", port_name_in_0)
+print("Midi Input from TotalMix:", port_name_in_1)
 print("Midi Output: {}\n".format(port_name_out))
+
+#Get LCD Text
+
+midiout.send_message([0x80, 0x28, 0])
+
+while True:
+
+    msg1 = midiin_1.get_message()
+
+    if msg1:
+
+        message, deltatime = msg1
+
+        if lcd_header == message[:6]:
+
+            ascii_char = ""
+            for num in message[7:-1]: ascii_char += chr(num)
+
+
+            print("start ascii_char", ascii_char, "\n")
+
+            break
+
+    time.sleep(0.0025)
+
+#Main
 
 try:
 
@@ -168,8 +252,8 @@ try:
                                 output_ch = int(output_CH_dict[key])
                                 submix = [int(ele, 16) for ele in output_submix_dict[value].split(",")]
 
-                                if value == "Send":
-
+                                if value == "TlkB":
+                                    #print("-------------if TlkB")
                                     output_CC_or_Note = int(output_CC_dict[key], 16)
                                     print(message)
                                     output_type = "Note_Off"
@@ -193,7 +277,7 @@ try:
 
 
                                 elif value == "Map":
-
+                                    #print("---------------elif Map")
                                     output_CC_or_Note = int(output_CC_dict[key], 16)
                                     output_type = "Note_Off"
                                     output_value = message[2]
@@ -207,11 +291,13 @@ try:
                                     #print("Map to {}".format(key))
 
                                 elif value == "S" or value == "M":
-
+                                    #print("---------------elif S or M")
                                     totalmix_row = output_LCD_dict[key]
                                     output_ch = 0
 
-                                    banking(totalmix_row)
+                                    print("before banking:", ascii_char)
+                                    ascii_char = banking(totalmix_row,ascii_char)
+                                    print("after banking:", ascii_char)
 
                                     if value == "S": output_CC_or_Note = int(output_solo_dict[key], 16)
 
@@ -237,7 +323,7 @@ try:
 
 
                                 else:
-
+                                    print("---------------else")
                                     output_CC_or_Note = int(output_CC_dict[key])
                                     output_type = "CC"
                                     output_value = message[2]
@@ -251,40 +337,22 @@ try:
                                 print("Submix -> ", value)
 
                                 if send:
-                                    midiout.send_message(msgout)
                                     print("Output -> {} Ch: {} {}: {} Value: {} \n".format(key, output_ch, output_type, output_CC_or_Note, output_value))
+                                    midiout.send_message(msgout)
 
 
                         break
-        if msg1:
 
-            message, deltatime = msg1
+        #if msg1:
 
+            #message, deltatime = msg1
 
+            #if lcd_header == message[:6]:
 
-            #if message == [159, 127, 90]:
-                #print("Midi In 2:", message, "<--Feedback Detection Pulse")
-
-
-            if lcd_header == message[:6]:
-
-                #sysex_msg_hex = []
-                #for byte in message:
-                #    sysex_msg_hex.append(hex(byte))
-                #print("Midi In 2:", sysex_msg_hex, "<- -SYSEX")
-
-                ascii_char = ""
-                for num in message[7:-1]:
-                    ascii_char += chr(num)
-                print("LCD Text -> ", ascii_char, "\n")
-
-
-            #else:
-            #    print("Midi In 2:", message)
-
-
-
-
+                #ascii_char = ""
+                #for num in message[7:-1]:
+                 #   ascii_char += chr(num)
+                #print("LCD Text -> ", ascii_char, "\n")
 
     time.sleep(0.0025)
 
